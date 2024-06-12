fd606c4d2a0627966e2aa6848fa516fc02246a4c
cuplprojects <shivom.chandrakala@gmail.com>
2024-06-12 12:19:07 +0530

    Modified AppDbContext

--- SPA/Data/AppDbContext.cs
+++ SPA/Data/AppDbContext.cs
@@ -1,5 +1,5 @@
-﻿using SPA.Models;
-using SPA.Models.NonDBModels;
+﻿using Microsoft.EntityFrameworkCore;
+using SPA.Models;
 
 namespace SPA.Data
 {
@@ -10,19 +10,19 @@ namespace SPA.Data
         {
         }
         public DbSet <User> Users { get; set; }
-        public DbSet <Login> Login { get; set; }
+        public DbSet <UserAuth> UserAuths { get; set; }
         public DbSet <Role> Roles { get; set; }
         public DbSet <Modules> Module {  get; set; }
         public DbSet <Score> Scores { get; set; }
         public DbSet <Reports> Report {  get; set; }
-        public Dbset <ResponseConfig> ResponseConfig { get; set; }
+        public DbSet <ResponseConfig> ResponseConfigs { get; set; }
         public DbSet <RegistrationData> RegistrationData { get; set; }
         public DbSet <Project> Projects { get; set; }
         public DbSet <Permission> Permissions { get; set; }
         public DbSet <OMRdata> OMRdata { get; set; }
         public DbSet <CorrectedOMRData> CorrectedOMRData { get; set; }
         public DbSet <Keys> Key {  get; set; }  
-        public Dbset <ImageConfig> ImageConfigs { get; set; }
+        public DbSet <ImageConfig> ImageConfigs { get; set; }
         public DbSet <Absentee> Absentees {  get; set; }
         public DbSet <AmbiguousQues> AmbiguousQue {  get; set; }
         public DbSet <Field> Fields { get; set; }
