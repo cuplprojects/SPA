50f851f6aa3e1facd59bb5e2cbeb6d52045ba213
cuplprojects <shivom.chandrakala@gmail.com>
2024-06-12 12:24:41 +0530

    changing

--- SPA/Data/AppDbContext.cs
+++ SPA/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using SPA.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SPA.Models;
+using System.Collections.Generic;
 
 namespace SPA.Data
 {
@@ -8,25 +10,26 @@ namespace SPA.Data
             : base(options)
         {
         }
-        public DbSet <User> Users { get; set; }
-        public DbSet <Login> Login { get; set; }
-        public DbSet <Role> Roles { get; set; }
-        public DbSet <Modules> Module {  get; set; }
-        public DbSet <Score> Scores { get; set; }
-        public DbSet <Reports> Report {  get; set; }
-        public Dbset <ResponseConfig> ResponseConfig { get; set; }
-        public DbSet <RegistrationData> RegistrationData { get; set; }
-        public DbSet <Project> Projects { get; set; }
-        public DbSet <Permission> Permissions { get; set; }
-        public DbSet <OMRdata> OMRdata { get; set; }
-        public DbSet <CorrectedOMRData> CorrectedOMRData { get; set; }
-        public DbSet <Keys> Key {  get; set; }  
-        public Dbset <ImageConfig> ImageConfigs { get; set; }
-        public DbSet <Absentee> Absentees {  get; set; }
-        public DbSet <AmbiguousQues> AmbiguousQue {  get; set; }
-        public DbSet <Field> Fields { get; set; }
-        public DbSet <Flags> Flag { get; set; }
-        public DbSet <FieldConfiguration> FieldsConfig { get; set; }
-        public DbSet <MarkingRule> MarkingRules { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Login> Login { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Modules> Module { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Reports> Report { get; set; }
+        public DbSet<ResponseConfig> ResponseConfig { get; set; }
+        public DbSet<RegistrationData> RegistrationData { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<OMRdata> OMRdata { get; set; }
+        public DbSet<CorrectedOMRData> CorrectedOMRData { get; set; }
+        public DbSet<Keys> Key { get; set; }
+        public DbSet<ImageConfig> ImageConfigs { get; set; }
+        public DbSet<Absentee> Absentees { get; set; }
+        public DbSet<AmbiguousQues> AmbiguousQue { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Flags> Flag { get; set; }
+        public DbSet<FieldConfiguration> FieldsConfig { get; set; }
+        public DbSet<MarkingRule> MarkingRules { get; set; }
     }
 }
---
+++ SPA/Models/ChangePassword.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SPA.Models
+{
+    public class ChangePassword
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
